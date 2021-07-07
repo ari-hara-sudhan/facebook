c908cde4a8e4154b46b6c7db108005552f7525fe
=ari <=ahshari2003@gmail.com>
2021-07-07 09:31:16 +0530

    added sidebar & sidebarrow

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -11,7 +11,40 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SidebarRow/>
+            <SidebarRow 
+            src="https://www.bing.com/th?id=OIP.5TFdq1ah-13yRK4aqMqt8wHaFj&w=196&h=160&c=8&rs=1&qlt=90&o=6&dpr=1.25&pid=3.1&rm=2" title="ahs dev"
+            />
+             <SidebarRow 
+             Icon={LocalHospitalIcon}
+             title="Covid-19 Vacination"
+            />
+            <SidebarRow 
+             Icon={EmojiEmotionsIcon}
+             title="Face"
+            />
+            <SidebarRow 
+             Icon={PeopleIcon}
+             title="Friends"
+            />
+            <SidebarRow 
+             Icon={ChatIcon}
+             title="Message"
+            />
+            <SidebarRow 
+             Icon={StoreIcon}
+             title="Marketplace"
+            />
+            <SidebarRow 
+             Icon={VideoLibraryIcon}
+             title="Videos"
+            />
+            <SidebarRow 
+             Icon={ExpandMoreIcon}
+             title="Showmore"
+            />
+         
+           
+            
             
         </div>
     )
--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import "./SidebarRow.css"
+import { Avatar } from '@material-ui/core'
 
-function SidebarRow() {
+function SidebarRow({src,Icon,title}) {
     return (
         <div className="sidebarrow">
+            {src && <Avatar  src={src}/>}
+            {Icon && <Icon fontSize="large"/>}
+            <h4 className="sidebarrow__title">{title}</h4>
             
         </div>
     )
